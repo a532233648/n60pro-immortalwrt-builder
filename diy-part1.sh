757169ba50b71e4cc0d8ad04f25b5a8d5b15703b
a532233648 <b532233648@gmail.com>
2026-07-11 14:44:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
+set -e
+
 #=================================================
-# 在更新 feeds 之前执行
-# 用于添加或修改第三方软件源
+# 添加 OpenClash 源码
 #=================================================
 
-# 第一次编译建议保持为空，先确认基础固件能够正常生成。
+rm -rf package/luci-app-openclash
+rm -rf /tmp/OpenClash
+
+git clone \
+  --depth 1 \
+  https://github.com/vernesong/OpenClash.git \
+  /tmp/OpenClash
+
+cp -rf \
+  /tmp/OpenClash/luci-app-openclash \
+  package/
+
+rm -rf /tmp/OpenClash
 
 
-# 添加第三方软件源示例：
+#=================================================
+# 添加其他第三方软件源示例
 #
+# 暂时不需要时保持注释即可
+#=================================================
+
 # echo 'src-git mypackages https://github.com/用户名/仓库名.git;main' \
-# >> feeds.conf.default
-
-
-# 添加多个软件源示例：
-#
-# echo 'src-git packages1 https://github.com/用户名/仓库1.git;main' \
-# >> feeds.conf.default
-#
-# echo 'src-git packages2 https://github.com/用户名/仓库2.git;main' \
 # >> feeds.conf.default
